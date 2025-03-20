4f80e34f907fdc64f911a78a3b1c5074350f8873
Daniel-J-Anaya <ana15001@byui.edu>
2025-03-20 12:47:11 -0600

    Updated App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,20 +1,10 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
   import Counter from './lib/Counter.svelte'
   import InputForm from './lib/InputForm.svelte';
 </script>
 
 <main>
-  <div>
-    <a href="https://vite.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
+  <h1>Personal Finance Tracker</h1>
 
   <div class="card">
     <Counter />
