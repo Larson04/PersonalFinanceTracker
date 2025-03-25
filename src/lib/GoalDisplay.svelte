c845558d1ab643aad94ec57352e3d25af7b54422
Andre Regino <reginopanai@gmail.com>
2025-03-25 00:52:52 -0600

    refactoring goal displayer to create as many sections with color as possible

--- src/lib/GoalDisplay.svelte
+++ src/lib/GoalDisplay.svelte
@@ -1,43 +1,12 @@
 <script>
 
-let gpaCols = {"4.00": 0, "3.95": 1,"3.90": 2, "3.85": 3, "3.80": 4, "3.75": 5,"3.70": 6, "3.65": 7, "3.60": 8, "3.55": 9, "3.50": 10}
-let actRows= {"36": 0, "35": 1, "34": 2, "33": 3, "32": 4, "31": 5, "30": 6, "29": 7, "28": 8, "27": 9, "26": 10,"25": 11}
-
-let scholarshipMatrix = [ 
-  //  0   1   2   3   4   5   6   7   8   9  10
-    [100,100,100,100,100,100,100,100,100,100,100], //0
-    [100,100,100,100,100,100,100,100,100,50, 50],  //1
-    [100,100,100,100,100,100,100,100,50, 50, 50],  //2
-    [100,100,100,100,100,100,100,50, 50, 50, 50],  //3
-    [100,100,100,100,100,100,50, 50, 50, 50, 25],  //4
-    [100,100,100,100,100,50, 50, 50, 50, 25, 25],  //5
-    [100,100,100,100,50, 50, 50, 50, 25, 25, 25],  //6
-    [100,50, 50, 50, 50, 50, 50, 25, 25, 25, 25],  //7
-    [50, 50, 50, 50, 50, 50, 25, 25, 25, 25, 25],  //8
-    [50, 50, 50, 50, 50, 25, 25, 25, 25, 25, 25],  //9
-    [50, 50, 50, 50, 25, 25, 25, 25, 25, 25, 25],  //10
-    [50, 50, 50, 25, 25, 25, 25, 25, 25, 25, 25],  //11
-    [25, 25, 25, 25, 25,  0,  0,  0,  0,  0,  0]   //12
-]
-
-
 function getPercentages(){
-  let row = 6
-  let col = 5
-  return scholarshipMatrix[row][col]
+  return 30
 }
 
-function setCSSAnimation(meritPercentage, rmPercentage){
+function setCSSAnimation(goalPercentages){
   
   const styleSheet = document.styleSheets[1]; 
-
-  // // Remove any existing keyframes rule with the same name
-  // for (let i = 0; i < styleSheet.cssRules.length; i++) {
-  //   if (styleSheet.cssRules[i].name === 'progress') {
-  //     styleSheet.deleteRule(i);
-  //     break;
-  //   }
-  // }
 
   // Create the new keyframes rule
   const keyframesRule = `
@@ -47,6 +16,7 @@ function setCSSAnimation(meritPercentage, rmPercentage){
         --RM: 0%;
       }
       to {
+        ${goalPercentages}
         --merit: ${meritPercentage}%;
         --RM: ${meritPercentage + rmPercentage}%;
       }
@@ -71,20 +41,21 @@ function resetAnimation(element) {
 
 function calculateScholarship(){
   
+  let goalPercentages = {}
 
 
   const tuition = 2400 
   const tuitionPercentage = 100
   const meritPercentage = getPercentages()
+  goalPercentages['merit'] = meritPercentage
   const merit =  (meritPercentage * tuition)/tuitionPercentage
   const rMissionary = 1000 
-  const rmPercentage = (rMissionary * tuitionPercentage)/tuition
+  const rmPercentage = (rMissionary * tuitionPercentage)/tuition  
+  goalPercentages['RM'] = rmPercentage
+  
+  
 
-  let totalPay = tuition - merit - rMissionary
-  let balance = (totalPay > 0) ? totalPay : 0
-  let excess = (totalPay < 0) ? totalPay * -1 : 0
-  
-  
+
   const circle = document.querySelector('.circle');
   if (circle) {
     // @ts-ignore
@@ -93,21 +64,15 @@ function calculateScholarship(){
     circle.style.setProperty('--merit', `${meritPercentage}%`);
     // @ts-ignore
     circle.style.setProperty('--RM', `${rmPercentage}%`);
+
+    // circle.computedStyleMap.setProperty('backgroun')
     // @ts-ignore
     circle.style.animation = ''; 
   }
-
-
-  setCSSAnimation(meritPercentage, rmPercentage)
+  setCSSAnimation(goalPercentages)
 
 
 }
-
-
-const results =  document.querySelector('.results')
-const pending =  document.querySelector('.pending')
-const resultsContainer = document.querySelector(".resultsContainer")
-const calculator = document.querySelector(".calculator")
 
 
 setTimeout(calculateScholarship,3000)
@@ -118,25 +83,8 @@ setTimeout(calculateScholarship,3000)
 
 // document.querySelector('#startOver').addEventListener("click", ()=>{
 
-//   document.getElementById('calculatorForm').reset()
 //   resetAnimation(document.querySelector('.circle'))
 
-//   if (calculator.classList.contains('hidden')){
-//     calculator.classList.remove('hidden')
-//     resultsContainer.classList.add('hidden')
-//   }else{
-//     results.classList.add('hidden')
-//     pending.classList.remove('hidden')
-//   }
-
-//   if(window.innerWidth < 1000){
-//     resultsContainer.style.visibility = 'hidden' 
-//     // calculator.classList.add('hidden')
-//     // resultsContainer.style.visibility = 'visible'
-//     // resultsContainer.classList.remove('hidden')
-//     resultsContainer.style.height = '0';
-//     resultsContainer.style.width = '0';
-//   }
 // })
 
 
@@ -148,8 +96,8 @@ setTimeout(calculateScholarship,3000)
 <div class="circleContainer">
     <div class="circle"></div>
     <div class="textWrapper">
-      <p>Total Scholarships</p>
-      <span class="totalScholarships">$0</span>
+      <p>Goal Progress</p>
+      <span class="totalProgress">$0</span>
     </div>
 </div>
 
@@ -167,17 +115,17 @@ setTimeout(calculateScholarship,3000)
   initial-value: 0%;
 }
 
-@keyframes progress {
+/* @keyframes progress {
   from {
     --merit: 0%;
-    --RM: 50%
+    --RM: 0%
   }
 
   to {
     --merit: 50%;
     --RM: 75%
   }
-}
+} */
 
 .circle {
   --merit: 0%;
@@ -185,11 +133,11 @@ setTimeout(calculateScholarship,3000)
  
   background: conic-gradient(
     at center,
-    var(--merit-color) var(--merit),
+    red var(--merit),
     calc(var(--merit)),
-    var(--RM-color) calc(var(--RM)),
+    blue calc(var(--RM)),
     calc(var(--RM)),
-    var(--total-color) calc(var(--merit))
+    rgb(220, 217, 217) calc(var(--merit))
   );
   animation: progress 2s linear 1 forwards;
   border-radius: 50%;
@@ -199,7 +147,7 @@ setTimeout(calculateScholarship,3000)
 }
 
 .circleContainer {
-  filter: drop-shadow(0 0 0.8vmin hsla(0, 0, 0%, 1));
+  /* filter: drop-shadow(0 0 0.8vmin hsla(0, 0, 0%, 1)); */
   position: relative;
   width: 200px;
   margin: 15px;
@@ -232,7 +180,7 @@ setTimeout(calculateScholarship,3000)
   margin: .3rem ;
 }
 
-.totalScholarships{
+.totalProgress{
   color: #363636;
   text-align: center;
   font-size: 35px;
