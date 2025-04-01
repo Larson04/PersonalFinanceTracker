a96cf72e6b7dedc888965a823696e8d271c52e8c
Andre Regino <reginopanai@gmail.com>
2025-04-01 12:46:08 -0600

    updates to goal Display component

--- src/lib/GoalDisplay.svelte
+++ src/lib/GoalDisplay.svelte
@@ -1,107 +1,107 @@
 <script>
   import { getLocalStorage } from "../js/utils";
 
-  function getAllGoalsStart(goals){
+  // function getAllGoalsStart(goals){
 
-    let progress = ''
+  //   let progress = ''
 
-    Object.keys(goals).forEach((key) => {
+  //   Object.keys(goals).forEach((key) => {
 
-      progress += `\n --${key}: 0%;`
-    })
-    return progress
-  }
+  //     progress += `\n --${key}: 0%;`
+  //   })
+  //   return progress
+  // }
 
-  function getAllGoalsEnd(goals){
+  // function getAllGoalsEnd(goals){
 
-    let progress = ''
+  //   let progress = ''
 
-    Object.entries(goals).forEach(([key, value]) => {
+  //   Object.entries(goals).forEach(([key, value]) => {
 
-      progress += `\n --${key}: ${value}%;`
-    })
-    return progress
-  }
+  //     progress += `\n --${key}: ${value}%;`
+  //   })
+  //   return progress
+  // }
 
-  function setCSSAnimation(goalPercentages){
+  // function setCSSAnimation(goalPercentages){
     
-    const styleSheet = document.styleSheets[1]; 
+  //   const styleSheet = document.styleSheets[1]; 
 
-    // Create the new keyframes rule
-    const keyframesRule = `
-      @keyframes progress {
-        from {
-          ${getAllGoalsStart(goalPercentages)}
-        }
-        to {
-          ${getAllGoalsEnd(goalPercentages)}
-        }
-      }
-    `;
+  //   // Create the new keyframes rule
+  //   const keyframesRule = `
+  //     @keyframes progress {
+  //       from {
+  //         ${getAllGoalsStart(goalPercentages)}
+  //       }
+  //       to {
+  //         ${getAllGoalsEnd(goalPercentages)}
+  //       }
+  //     }
+  //   `;
 
-    // Inject the new keyframes rule
-    styleSheet.insertRule(keyframesRule, styleSheet.cssRules.length);
+  //   // Inject the new keyframes rule
+  //   styleSheet.insertRule(keyframesRule, styleSheet.cssRules.length);
 
-  }
-
-
-
-  function resetAnimation(element) {
-    element.style.animation = 'none'; 
-    element.offsetHeight; 
-    element.style.setProperty('--tuition', '0%');
-    element.style.setProperty('--merit', '0%');
-    element.style.setProperty('--RM', '0%');
+  // }
 
 
-  }
 
-  function getTotalPercentages(goals){
+  // function resetAnimation(element) {
+  //   element.style.animation = 'none'; 
+  //   element.offsetHeight; 
+  //   element.style.setProperty('--tuition', '0%');
+  //   element.style.setProperty('--merit', '0%');
+  //   element.style.setProperty('--RM', '0%');
 
-    let percentages = {} 
 
-    const monthlyBudget = goals[0]['allowedExpense'] 
-    const budgetPercentage = 100
+  // }
+
+  // function getTotalPercentages(goals){
+
+  //   let percentages = {} 
+
+  //   const monthlyBudget = goals[0]['allowedExpense'] 
+  //   const budgetPercentage = 100
     
-    goals.forEach((item) => {
+  //   goals.forEach((item) => {
       
-      let goalPercentage = (item['currentExpense'] * budgetPercentage ) / monthlyBudget
+  //     let goalPercentage = (item['currentExpense'] * budgetPercentage ) / monthlyBudget
 
-      percentages[item['category']] = goalPercentage
+  //     percentages[item['category']] = goalPercentage
 
-    })
+  //   })
     
-    percentages['monthlyBudget'] = budgetPercentage
+  //   percentages['monthlyBudget'] = budgetPercentage
 
-    return percentages
-  }
+  //   return percentages
+  // }
 
-  function calculateGoals(){
+  // function calculateGoals(){
 
-    let goals = getLocalStorage('goals')
+  //   let goals = getLocalStorage('goals')
     
-    let goalPercentages = getTotalPercentages(goals)
+  //   let goalPercentages = getTotalPercentages(goals)
 
-    console.log(goalPercentages)
+  //   console.log(goalPercentages)
 
-    const circle = document.querySelector('.circle');
-    if (circle) {
+  //   const circle = document.querySelector('.circle');
+  //   if (circle) {
 
-      Object.entries(goalPercentages).forEach(([key, value]) => {
-        circle.style.setProperty(`--${key}`, `${value}%`);
-      });
+  //     Object.entries(goalPercentages).forEach(([key, value]) => {
+  //       circle.style.setProperty(`--${key}`, `${value}%`);
+  //     });
 
-      // circle.computedStyleMap.setProperty('background')
-      // @ts-ignore
-      circle.style.animation = ''; 
-    }
-    setCSSAnimation(goalPercentages)
-
-
-  }
+  //     // circle.computedStyleMap.setProperty('background')
+  //     // @ts-ignore
+  //     circle.style.animation = ''; 
+  //   }
+  //   setCSSAnimation(goalPercentages)
 
 
-  setTimeout(calculateGoals,1000)
+  // }
+
+
+  // setTimeout(calculateGoals,1000)
 
 
 
@@ -139,6 +139,106 @@
   //   {...}
   // ]
 
+
+  function getAllGoalsStart(goals) {
+    let progress = '';
+
+    Object.keys(goals).forEach((key) => {
+      progress += `\n --${key}: 0%;`;
+    });
+    return progress;
+  }
+
+  function getAllGoalsEnd(goals) {
+    let progress = '';
+
+    Object.entries(goals).forEach(([key, value]) => {
+      progress += `\n --${key}: ${value}%;`;
+    });
+    return progress;
+  }
+
+  function setCSSAnimation(goalPercentages) {
+    const styleSheet = document.styleSheets[1];
+
+    // Create the new keyframes rule dynamically
+    const keyframesRule = `
+      @keyframes progress {
+        from {
+          ${getAllGoalsStart(goalPercentages)}
+        }
+        to {
+          ${getAllGoalsEnd(goalPercentages)}
+        }
+      }
+    `;
+    
+    // Inject the new keyframes rule
+    styleSheet.insertRule(keyframesRule, styleSheet.cssRules.length);
+  }
+
+  function getTotalPercentages(goals) {
+    let percentages = {};
+    const monthlyBudget = goals[0]['allowedExpense'];
+    const budgetPercentage = 100;
+    
+    goals.forEach((item) => {
+      let goalPercentage = (item['currentExpense'] * budgetPercentage) / monthlyBudget;
+      percentages[item['category']] = goalPercentage;
+    });
+    
+    percentages['monthlyBudget'] = budgetPercentage;
+
+    return percentages;
+  }
+
+  function calculateGoals() {
+  let goals = getLocalStorage('goals');
+  let goalPercentages = getTotalPercentages(goals);
+
+  console.log(goalPercentages);
+
+  const circle = document.querySelector('.circle');
+  if (circle) {
+    // 1. Create and inject the keyframes rule before applying animation
+    setCSSAnimation(goalPercentages);
+
+    // 2. Set an initial "empty" gradient (everything at 0%)
+    let initialGradient = '';
+    Object.entries(goalPercentages).forEach(([key, value], index) => {
+      if (key !== 'monthlyBudget') {
+        initialGradient += `lightgray 0% 0%, `;
+      }
+    });
+
+    circle.style.background = `conic-gradient(at center, ${initialGradient.slice(0, -2)})`;
+
+    // 3. Delay applying the animated gradient to allow the keyframes to take effect
+    setTimeout(() => {
+      let newGradient = '';
+      let startAngle = 0;
+
+      Object.entries(goalPercentages).forEach(([key, value], index) => {
+        if (key !== 'monthlyBudget') {
+          let endAngle = startAngle + value;
+          newGradient += `${getColorForGoal(index)} ${startAngle}% ${endAngle}%, `;
+          startAngle = endAngle;
+        }
+      });
+
+      // 4. Apply the final conic-gradient and start the animation
+      circle.style.background = `conic-gradient(at center, ${newGradient.slice(0, -2)})`;
+      circle.style.animation = 'progress 2s linear 1 forwards';
+    }, 100); // Short delay allows animation to start smoothly
+  }
+}
+
+  function getColorForGoal(index) {
+    const colors = ['red', 'blue', 'green', 'yellow', 'orange']; // Colors for goals
+    return colors[index % colors.length]; // Cycle through colors
+  }
+
+  setTimeout(calculateGoals, 1000);
 
 </script>
 
@@ -178,7 +278,7 @@
 } */
 
 .circle {
-  --merit: 0%;
+  /* --merit: 0%;
   --RM: 0%;
  
   background: conic-gradient(
@@ -188,7 +288,8 @@
     blue calc(var(--RM)),
     calc(var(--RM)),
     rgb(220, 217, 217) calc(var(--merit))
-  );
+  ); */
+  background: conic-gradient(at center, #dcd9d9 0deg); /* Placeholder */
   animation: progress 2s linear 1 forwards;
   border-radius: 50%;
   mask: radial-gradient(circle at center, transparent 55%, black 55%);
