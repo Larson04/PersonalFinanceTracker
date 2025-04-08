0ffa55624c7bc1d497fbae7fc8f4c8d1ba2afb0d
Daniel-J-Anaya <ana15001@byui.edu>
2025-04-08 12:48:29 -0600

    updated App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,19 +1,10 @@
 <script>
   import InputForm from './lib/InputForm.svelte';
-  import TransactionTable from './lib/TransactionTable.svelte';
   import Hero from './lib/Hero.svelte';
-  import GoalDisplay from './lib/GoalDisplay.svelte';
-  import { getLocalStorage } from './js/utils.js';
   import GoalList from './lib/GoalList.svelte';
-  import TransactionEntry from './lib/TransactionEntry.svelte';
   import TableContainer from './lib/TableContainer.svelte';
   import GoalForm from './lib/GoalForm.svelte';
   import WelcomeBanner from './lib/WelcomeBanner.svelte';
-
-  const months = ['January', 'February', 'March', 'April', 'May', 'June', 
-  'July', 'August', 'September', 'October', 'November', 'December'];
-  
-
 </script>
 
 <header id="header">
